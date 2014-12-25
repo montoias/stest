962a3c8cbd8184cc3916c4f58a13667eaea6f7ec
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2014-12-25 05:40:00 +0000

    Add SimpleSearch Class
    Constructor does not associate any object to any given key

---
+++ src/Uniplaces/STest/Search/SimpleSearch/SimpleSearch.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montoias
+ * Date: 24/12/14
+ * Time: 19:11
+ */
+
+namespace Uniplaces\STest\Search\SimpleSearch;
+
+use Uniplaces\STest\Search\SearchOperations;
+
+class SimpleSearch extends SearchOperations {
+
+    public function __construct()
+    {
+
+    }
+
+}
