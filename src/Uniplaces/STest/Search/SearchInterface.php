08023313710c7c519ff11cd91eb8780174416245
Paulo Monteiro <prrm@kdbio.inesc-id.pt>
2014-12-25 05:26:14 +0000

    Create interface to establish a common search method

---
+++ src/Uniplaces/STest/Search/SearchInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montoias
+ * Date: 25/12/14
+ * Time: 04:46
+ */
+
+namespace Uniplaces\STest\Search;
+
+
+interface SearchInterface {
+
+    public function execute($listing, $search);
+
+}
